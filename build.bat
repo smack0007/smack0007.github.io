26bf8be5fae25fcd4bca91c85124fb3e0c604e2a
smack0007 <zachary.snow@gmail.com>
2017-11-13 13:59:07 +0100

    Commit done on server takes message from source commit.

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 PUSHD %~dp0
+
 IF EXIST bin (
     RMDIR /s /q bin
 )
@@ -7,11 +8,15 @@ IF EXIST bin (
 git clone "https://github.com/smack0007/smack0007.github.io.git" bin
 dotnet build .\compiler\compiler.csproj -p:BuildSite=true
 
+git log -1 --pretty=%B > commit.msg
+
 PUSHD bin
 git config user.name "smack0007"
 git config user.email zachary.snow@gmail.com
-git add -A && git commit -m "Build Process"
+git add -A && git commit -F "..\commit.msg"
 POPD
+
+DEL commit.msg
 
 POPD
 
