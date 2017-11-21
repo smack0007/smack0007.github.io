e29318affdfc93a476c3a9b1f40b779d8dac6aa4
smack0007 <zachary.snow@gmail.com>
2017-11-21 15:30:12 +0100

    Small visual improvements.

--- compiler/PostTemplate.cs
+++ compiler/PostTemplate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace compiler
 {
@@ -21,10 +22,8 @@ namespace compiler
             yield return "<div class=\"meta\">";
                 yield return $"<span class=\"date\"><i class=\"fa fa-calendar\"></i>{data.Date}</span>";
                 yield return $"<span class=\"category\"><i class=\"fa fa-folder\"></i>{HtmlEncode(data.Category)}</span>";
-                foreach (var tag in data.Tags)
-                {
-                    yield return $"<span class=\"tags\"><i class=\"fa fa-tags\"></i>{HtmlEncode(tag)}</span>";
-                }
+                var tags = data.Tags.Select(x => HtmlEncode(x));
+                yield return $"<span class=\"tags\"><i class=\"fa fa-tags\"></i>{string.Join(", ", tags)}</span>";
             yield return "</div>";
         }
     }
