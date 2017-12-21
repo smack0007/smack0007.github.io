3a1eda2abb4612f51096c8079dd4fe7e96caeab8
smack0007 <zachary.snow@gmail.com>
2017-12-21 11:15:43 +0100

    Switched to Poole / Lanyon for design.

--- compiler/PageTemplate.cs
+++ compiler/PageTemplate.cs
@@ -17,37 +17,46 @@ $@"<!doctype html>
         <meta name=""author"" content=""Zachary Snow"">
 
         <title>{data.Title}</title>
-        <link href=""https://fonts.googleapis.com/css?family=Open+Sans"" rel=""stylesheet"">
+        <link rel=""stylesheet"" href=""http://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700|PT+Sans:400"">
         <link rel=""stylesheet"" type=""text/css"" href=""{data.BasePath}css/site.css"">
 
         <link rel=""alternate"" type=""application/rss+xml"" title=""The Blog of Zachary Snow"" href=""{data.BasePath}feed.rss"">
     </head>
     <body>
-        <header>
-            <nav>
-                <div class=""container"">
-                    <a href=""{data.BasePath}index.html"" {If(data.FileName == "index.html", "class=\"active\"")}>Home</a>
-                    <a href=""{data.BasePath}about.html"" {If(data.FileName == "about.html", "class=\"active\"")}>About</a>
-                </div>
+        <input type=""checkbox"" class=""sidebar-checkbox"" id=""sidebar-checkbox"" />
+        <div class=""sidebar"" id=""sidebar"">
+            <div class=""sidebar-item"">
+
+            </div>
+            <nav class=""sidebar-nav"">
+                <a href=""{data.BasePath}"" class=""sidebar-nav-item{If(data.FileName == "index.html", " active")}"">Home</a>
+                <a href=""{data.BasePath}about.html"" class=""sidebar-nav-item{If(data.FileName == "about.html", " active")}"">About</a>
             </nav>
-            <div class=""container"">
-                <h1>The Blog of Zachary Snow</h1>
+        </div>
+        <div class=""wrap"">
+            <div class=""masthead"">
+                <div class=""container"">
+                    <div class=""social"">
+                        <a href=""http://twitter.com/smack0007"" class=""twitter"" title=""Twitter""><i class=""fa fa-twitter""></i></a>
+                        <a href=""http://github.com/smack0007"" class=""github"" title=""Github""><i class=""fa fa-github""></i></a>
+                        <a href=""{data.BasePath}feed.rss"" class=""rss"" title=""RSS""><i class=""fa fa-rss""></i></a>
+                    </div>
+                    <h3 class=""masthead-title"">
+                        <a href=""{data.BasePath}"" title=""Home"">The Blog of Zachary Snow</a>
+                    </h3>
+                </div>
             </div>
-        </header>
-        <main class=""container"">
-            <div class=""posts"">
-                {data.Body}
+            <div class=""container content"">
+                <div class=""posts"">
+                    {data.Body}
+                </div>
+                <div class=""pagination"">
+                    <span class=""pagination-item older"">Older</span>
+                    <span class=""pagination-item newer"">Newer</span>
+                </div>
             </div>
-            <aside class=""sidebar"">
-				<div class=""social"">
-					<a href=""http://twitter.com/smack0007"" class=""twitter"" title=""Twitter""><i class=""fa fa-twitter""></i></a>
-					<a href=""http://github.com/smack0007"" class=""github"" title=""Github""><i class=""fa fa-github""></i></a>
-					<a href=""{data.BasePath}feed.rss"" class=""rss"" title=""RSS""><i class=""fa fa-rss""></i></a>
-				</div>
-            </aside>
-        </main>
-        <footer class=""container"">
-        </footer>
+        </div>
+        <label for=""sidebar-checkbox"" class=""sidebar-toggle""></label>
     </body>
 </html>";
         }
