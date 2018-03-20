6a37558fd39fa531fe4a7c1b0ab321016217ece0
smack0007 <zachary.snow@gmail.com>
2018-03-20 13:19:50 +0100

    Changing to VSTS for build process. (3)

--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
 git config --global credential.helper store
 git config --global push.default matching
-TYPE "https://%1:x-oauth-basic@github.com`n" > "%USERPROFILE%\.git-credentials"
+ECHO "https://%1:x-oauth-basic@github.com`n" > "%USERPROFILE%\.git-credentials"
 cd bin
 git push origin
