7c1b865f57e468c74c16b6836ff24af83d0e8115
smack0007 <zachary.snow@gmail.com>
2019-10-22 13:27:18 +0200

    Rename build.bat to publish.bat and merged repos together.

---
+++ publish.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+PUSHD %~dp0
+
+IF EXIST bin (
+    RMDIR /s /q bin
+)
+
+git clone "https://github.com/smack0007/smack0007.github.io.git" bin
+dotnet build .\compiler\compiler.csproj -p:BuildSite=true
+
+git log -1 --pretty=%%B > commit.msg
+
+PUSHD bin
+git add -A && git commit -F "..\commit.msg"
+git push https://github.com/smack0007/smack0007.github.io.git head:master
+POPD
+
+DEL commit.msg
+
+POPD
+
+EXIT 0
