cd0e3993be9bd4927796af4662552a13835f45cc
smack0007 <zachary.snow@gmail.com>
2020-11-20 10:17:54 +0100

    Updating publish.bat to set user.name and user.email of cloned master branch.

--- publish.bat
+++ publish.bat
@@ -15,11 +15,14 @@ IF EXIST bin (
 )
 
 git clone "https://github.com/smack0007/smack0007.github.io.git" -b master bin
+
 dotnet build .\compiler\compiler.csproj -p:BuildSite=true
 
 git log -1 --pretty=%%B > commit.msg
 
 PUSHD bin
+git config user.name "smack0007"
+git config user.email "zachary.snow@gmail.com"
 git add -A && git commit -F "..\commit.msg"
 git push https://github.com/smack0007/smack0007.github.io.git head:master
 POPD
