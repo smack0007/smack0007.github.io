5648dacc0d0e182ee9cde91c88e6ef7af6cbe8da
zachary_snow_conciso <zachary.snow@conciso.de>
2021-06-01 15:06:40 +0200

    Cleaning out old files etc.

--- js/hello.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sayHello() {
-    console.info('Hello!');
-}
-exports.sayHello = sayHello;
--- js/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var hello_1 = require("./hello");
-hello_1.sayHello();
