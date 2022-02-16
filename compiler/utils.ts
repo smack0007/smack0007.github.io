726834f9c4c1ab74388c2f4bb077a9845b7ae716
Zachary Snow <zachary.snow@gmail.com>
2022-02-16 10:48:20 +0100

    Remove umask import.

--- compiler/utils.ts
+++ compiler/utils.ts
@@ -1,6 +1,5 @@
 import { copyFile, lstat, mkdir, readdir, readFile as _readFile, writeFile as _writeFile } from "fs/promises";
 import { extname, join } from "path";
-import { umask } from "process";
 
 export async function copyDirectory(src: string, dest: string): Promise<void> {
     await ensureDirectory(dest);
